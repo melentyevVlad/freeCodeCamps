f79e2901ebfc4879069daf6408e6382480964c2c
Vlad Melentyev <melentyev2397@gmail.com>
2022-06-21 03:52:42 +0300

    FCC

---
+++ FCC/50.js
@@ -0,0 +1,22 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true,
+    },
+    {
+      "artist": "Michael",
+      "title": "Zombie",
+      "release_year": 1998,
+      "formats": [
+        "online",
+        "CD",
+      ],
+    }
+  ];
