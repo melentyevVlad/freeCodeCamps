a0a0bfd91284128652f7091aaf498f356fd5ccb3
Vlad Melentyev <melentyev2397@gmail.com>
2022-06-24 00:36:44 +0300

    getset

---
+++ FCC/86.js
@@ -0,0 +1,16 @@
+class Thermostat {
+    constructor(Fahrenheit) {
+      this.Fahrenheit = Fahrenheit;
+    }
+    get temperature() {
+      return (5/9) * (this.Fahrenheit - 32);
+    } 
+    set temperature(Celsius) {
+      this.Fahrenheit = (Celsius * 9.0) / 5 + 32;
+    }
+  }
+  
+  const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+  let temp = thermos.temperature; // 24.44 in Celsius
+  thermos.temperature = 26;
+  temp = thermos.temperature; // 26 in Celsius
