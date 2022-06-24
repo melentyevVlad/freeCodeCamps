e03cbda7696a2668e7e104579ab533a9dfc10645
Vlad Melentyev <melentyev2397@gmail.com>
2022-06-24 17:29:23 +0300

    export

---
+++ FCC/87.js
@@ -0,0 +1,9 @@
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+  }
+  export {uppercaseString};
+  
+  const lowercaseString = (string) => {
+    return string.toLowerCase()
+  }
+  export {lowercaseString};
